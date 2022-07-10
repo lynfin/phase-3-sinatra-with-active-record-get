80930562f874abe22f7bc8c68423ae0fcab79ca8
Lyn <lynfinman@gmail.com>
2022-07-10 11:25:34 -0500

    retrieving games from database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < Sinatra::Base
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    # get all the games from the database
+    # return a JSON response with an array of all the game data
+    games = Game.all.order(:title)
+    games.to_json
   end
-
 end
